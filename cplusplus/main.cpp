4792539d80d71b03fb3ce2f716819fa331d8b5b2
zhujian <505169307@qq.com>
2019-08-19 10:39:43 +0800

    test(opencv): 添加opencv开发环境

--- cplusplus/main.cpp
+++ cplusplus/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    cv::Mat img = cv::imread("../lena.jpg");
+    cv::imshow("lena", img);
+    cv::waitKey(0);
+
     return 0;
 }
