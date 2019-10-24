2669e83a4cd39a709e539d4f22b9664c38475f61
zhujian <505169307@qq.com>
2019-10-24 17:01:26 +0800

    feat(graph): 分离邻接矩阵和邻接表的图实现

--- cplusplus/main.cpp
+++ cplusplus/main.cpp
@@ -1,5 +1,8 @@
 
-#include "undigraph_test.h"
+#include "AdjacencyTableUndirectedGraph.h"
+#include "AdjacencyMatrixUndirectedGraph.h"
+#include <memory>
+#include <cstring>
 
 std::array<VertexType, 9> vertexes = {"v0",
                                       "v1",
@@ -22,11 +25,107 @@ EdgeType arcs[9][9] = {
         {GINFINITY, 12,        8,         21,        GINFINITY, GINFINITY, GINFINITY, GINFINITY, 0}
 };
 
+//void MGraphUse(EdgeType (*arcs)[9]) {
+//    MGraph G;
+//    G.numVertexes = 9;
+//    G.numEdges = 15;
+//    G.vexs = {"v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"};
+//    int i = 0;
+//    for (auto &ary:G.arcs) {
+//        memcpy(&ary, arcs[i], sizeof(EdgeType) * 9);
+//        i++;
+//    }
+//
+//    AdjacencyMatrixUndirectedGraph graph;
+////    graph.CreateMGraph(&G);
+//    graph.PrintMGraph(G);
+//
+//    cout << "邻接矩阵 - 深度优先遍历" << endl;
+//    int *idxes = graph.DFSTraverse(G);
+//    for (i = 0; i < G.numVertexes; i++) {
+//        cout << G.vexs[idxes[i]] << " ";
+//    }
+//    cout << endl;
+//
+//    cout << "邻接矩阵 - 广度优先遍历" << endl;
+//    idxes = graph.BFSTraverse(G);
+//    for (i = 0; i < G.numVertexes; i++) {
+//        cout << G.vexs[idxes[i]] << " ";
+//    }
+//    cout << endl;
+//
+//    cout << "邻接矩阵 - 最小生成树 - Prim" << endl;
+//    graph.MiniSpanTree_Prim(G);
+//    cout << endl;
+//
+//    cout << "邻接矩阵 - 最小生成树 - Kruskal" << endl;
+//    graph.MiniSpanTree_Kruskal(G);
+//    cout << endl;
+//}
+
+void GraphAdjListUse(std::array<VertexType, 9> vertexes, EdgeType (*arcs)[9]) {
+    GraphAdjList G;
+    G.numVertexes = 9;
+    G.numEdges = 15;
+
+    int i, j;
+    EdgeNode *edgeNode;
+    for (i = 0; i < 9; i++) {
+        VertextNode vertextNode;
+        vertextNode.data = vertexes[i];
+        vertextNode.firstEdge = nullptr;
+        for (j = 0; j < 9; j++) {
+            if (i != j and arcs[i][j] != GINFINITY) {
+                edgeNode = (EdgeNode *) malloc(sizeof(EdgeNode));
+                edgeNode->adjvex = j;
+                edgeNode->weight = arcs[i][j];
+                edgeNode->next = vertextNode.firstEdge;
+                vertextNode.firstEdge = edgeNode;
+            }
+        }
+        G.adjList[i] = vertextNode;
+    }
+
+    AdjacencyTableUndirectedGraph graph;
+//    graph.CreateGraphAdjList(&G);
+    graph.PrintGraphAdjList(G);
+
+    cout << "邻接表 - 深度优先遍历" << endl;
+    int *idxes = graph.DFSTraverse(G);
+    for (i = 0; i < G.numVertexes; i++) {
+        cout << G.adjList[idxes[i]].data << " ";
+    }
+    cout << endl;
+
+    cout << "邻接表 - 广度优先遍历" << endl;
+    idxes = graph.BFSTraverse(G);
+    for (i = 0; i < G.numVertexes; i++) {
+        cout << G.adjList[idxes[i]].data << " ";
+    }
+    cout << endl;
+
+    cout << "邻接表 - 最小生成树 - Prim" << endl;
+    graph.MiniSpanTree_Prim(G);
+    cout << endl;
+
+    cout << "邻接表 - 最小生成树 - Kruskal" << endl;
+    graph.MiniSpanTree_Kruskal(G);
+    cout << endl;
+}
 
 int main() {
-    UndigraphTest undigraphTest;
-//    undigraphTest.MGraphUse(arcs);
-    undigraphTest.GraphAdjListUse(vertexes, arcs);
+    MGraph G;
+    G.numVertexes = 9;
+    G.numEdges = 15;
+    G.vexs = {"v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"};
+    int i = 0;
+    for (auto &ary:G.arcs) {
+        std::memcpy(&ary, arcs[i], sizeof(EdgeType) * 9);
+        i++;
+    }
+
+    AdjacencyMatrixUndirectedGraph graph;
+    graph.PrintMGraph(G);
 
     return 0;
 }
